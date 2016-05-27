b1f38f6c8b3e3f06ab43e81b00f7ae08cd253295
timothyvanderaerden <timothyvanderaerden@gmail.com>
2016-05-27 12:18:19 +0200

    ZIP

---
+++ BasicSecurity/App_Dev/Basic_Security_AppDev/Basic_Security_AppDev/Encrypteren.xaml.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Basic_Security_AppDev
+{
+    /// <summary>
+    /// Interaction logic for Encrypteren.xaml
+    /// </summary>
+    public partial class Encrypteren : Grid
+    {
+        public Encrypteren()
+        {
+            InitializeComponent();
+        }
+
+        private void fileButton_Click(object sender, RoutedEventArgs e)
+        {
+            fileTextBox.Text = SelectFile();
+        }
+
+        private void folderButton_Click(object sender, RoutedEventArgs e)
+        {
+            folderTextBox.Text = SelectFolder();
+        }
+
+        private void desButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                desTextBox.Text = DesUtility.GenerateKey();
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message + Environment.NewLine + ex.StackTrace, "Fout!");
+            }
+        }
+
+        private void myPrivateKeyFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            myPrivateKeyFileTextBox.Text = SelectFile("Private Key|*.privkey");
+        }
+
+        private void receiversPublicKeyFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            receiversPublicKeyFileTextBox.Text = SelectFile("Public Key|*.pubkey");
+        }
+
+        private void steganografieCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeEnabled();
+        }
+
+        private void steganografieCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ChangeEnabled();
+        }
+
+        private void imageButton_Click(object sender, RoutedEventArgs e)
+        {
+            imageTextBox.Text = SelectFile("Bitmap|*.bmp");
+        }
+
+        private void encryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+                string file = fileTextBox.Text;
+                string folder = folderTextBox.Text;
+                string desKey = desTextBox.Text;
+                string myPrivateKey = myPrivateKeyFileTextBox.Text;
+                string receiversPublicKey = receiversPublicKeyFileTextBox.Text;
+                string image = imageTextBox.Text;
+
+                if (!File.Exists(file) || !Directory.Exists(folder) || string.IsNullOrEmpty(desKey) || !File.Exists(myPrivateKey) || !File.Exists(receiversPublicKey) || ((bool)steganografieCheckBox.IsChecked && !File.Exists(image)))
+                {
+                    System.Windows.MessageBox.Show("Een van bovenstaande waarden is incorrect!");
+                }
+                else
+                {
+                    //// 1.1. Hash maken van file
+                    //RSAUtility.Hash(file, folder + "\\Hash.hash");
+                    // 1.2. Hash signen met private key sender (RSA)
+                    RSAUtility.SignHash(myPrivateKey, file, folder + "\\Hash.signed");
+                    //File.Delete(folder + "\\Hash.hash");
+
+                    // 2.1. File encrypteren met des sleutel
+                    DesUtility.EncryptFile(file, desKey, folder + "\\Encrypted.des");
+
+                    // 3.1. file maken met deskey en filename in
+                    StreamWriter generatedKey = new StreamWriter(folder + "\\Des.key");
+                    string fileName = System.IO.Path.GetFileName(file);
+                    generatedKey.WriteLine(fileName);
+                    generatedKey.WriteLine(desKey);
+                    generatedKey.Flush();
+                    generatedKey.Close();
+                    // 3.2. bovenstaande file encrypteren met public key receiver (RSA)
+                    RSAUtility.EncryptFile(folder + "\\Des.key", receiversPublicKey, folder + "\\Des.rsa");
+                    File.Delete(folder + "\\Des.key");
+
+                    // 4.1. steganografie toepassen
+                    if ((bool)steganografieCheckBox.IsChecked)
+                    {
+                        SteganografieUtility.embed(folder + "\\Encrypted.des", folder + "\\Des.rsa", folder + "\\Hash.signed", image, folder);
+                        File.Delete(folder + "\\Encrypted.des");
+                        File.Delete(folder + "\\Des.rsa");
+                        File.Delete(folder + "\\Hash.signed");
+                    }
+
+                    System.Windows.MessageBox.Show("Succesfully Encrypted!", "Succes");
+                    ClearFields();
+                }
+        //}
+        //    catch (Exception ex)
+        //    {
+        //        System.Windows.MessageBox.Show(ex.Message, "Error!");
+        //    }
+}
+
+        private string SelectFile(string filter = null)
+        {
+            string path = null;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (filter != null)
+                openFileDialog.Filter = filter;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+            }
+
+            return path;
+        }
+
+        private string SelectFolder()
+        {
+            string path = null;
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            dlg.ShowNewFolderButton = true;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                path = dlg.SelectedPath;
+            }
+            return path;
+        }
+
+        private void ChangeEnabled()
+        {
+            imageTextBox.Text = string.Empty;
+            imageTextBox.IsEnabled = !imageTextBox.IsEnabled;
+            imageButton.IsEnabled = !imageButton.IsEnabled;
+        }
+
+        private void ClearFields()
+        {
+            fileTextBox.Clear();
+            folderTextBox.Clear();
+            desTextBox.Clear();
+            myPrivateKeyFileTextBox.Clear();
+            receiversPublicKeyFileTextBox.Clear();
+            steganografieCheckBox.IsChecked = false;
+            imageTextBox.Clear();
+        }
+    }
+}
---
+++ BasicSecurity/App_Dev/Basic_Security_AppDev/Basic_Security_AppDev/RsaUtility.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Security_AppDev
+{
+    class RSAUtility
+    {
+        public static object Encode { get; private set; }
+
+        // Generate a new key pair
+        public static void Keys(string folder)
+        {
+            // Variables
+            CspParameters cspParams = null;
+            RSACryptoServiceProvider rsaProvider = null;
+            StreamWriter publicKeyFile = null;
+            StreamWriter privateKeyFile = null;
+            string publicKey = "";
+            string privateKey = "";
+            try
+            {
+                // Create a new key pair on target CSP
+                cspParams = new CspParameters();
+                cspParams.ProviderType = 1; // PROV_RSA_FULL
+                //cspParams.ProviderName; // CSP name
+                cspParams.Flags = CspProviderFlags.UseArchivableKey;
+                cspParams.KeyNumber = (int)KeyNumber.Exchange;
+                rsaProvider = new RSACryptoServiceProvider(cspParams);
+                // Export public ke
+                publicKey = rsaProvider.ToXmlString(false);
+                // Write public key to file
+                publicKeyFile = File.CreateText(folder+"\\Public.pubkey");
+                publicKeyFile.Write(publicKey);
+                // Export private/public key pair
+                privateKey = rsaProvider.ToXmlString(true);
+                // Write private/public key pair to file
+                privateKeyFile = File.CreateText(folder + "\\Private.privkey");
+                privateKeyFile.Write(privateKey);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                // Do some clean up if needed
+                if (publicKeyFile != null)
+                {
+                    publicKeyFile.Close();
+                }
+                if (privateKeyFile != null)
+                {
+                    privateKeyFile.Close();
+                }
+            }
+        } // Keys
+
+        //hash a file
+        public static void Hash(string fileToHash, string hashedFile)
+        {
+            try
+            {
+                SHA1Managed hash = new SHA1Managed();
+                byte[] plainBytes = null;
+                byte[] hashedBytes = null;
+
+                // Read the bytes from the file
+                plainBytes = File.ReadAllBytes(fileToHash);
+                // Hash the plain text
+                hashedBytes = hash.ComputeHash(plainBytes);
+                File.WriteAllBytes(hashedFile, hashedBytes);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //Sign a File with a private key
+        public static void SignHash(string privateKey, string fileToSign, string signedFile)
+        {
+            try
+            {
+                // Variables
+                CspParameters cspParams = null;
+                RSACryptoServiceProvider rsaProvider = null;
+                SHA1Managed hash = new SHA1Managed();
+                string privateKeyText = "";
+                byte[] plainBytes = null;
+                byte[] encryptedBytes = null;
+
+                // Select target CSP
+                cspParams = new CspParameters();
+                cspParams.ProviderType = 1; // PROV_RSA_FULL
+                                            //cspParams.ProviderName; // CSP name
+                rsaProvider = new RSACryptoServiceProvider(cspParams);
+                // Read public key from file
+                privateKeyText = File.ReadAllText(privateKey);
+                // Import public key
+                rsaProvider.FromXmlString(privateKeyText);
+                // Read hash text from file and turn it into bytes
+                plainBytes = File.ReadAllBytes(fileToSign);
+                // Encrypt the bytes
+                encryptedBytes = rsaProvider.SignHash(hash.ComputeHash(plainBytes), CryptoConfig.MapNameToOID("SHA1"));
+                // Write encrypted text to file
+                File.WriteAllBytes(signedFile, encryptedBytes);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //Remove a signature with the public key
+        public static bool CompareHashes(string publicKey, string signedFile, string hash)
+        {
+            try
+            {
+                // Variables
+                CspParameters cspParams = null;
+                RSACryptoServiceProvider rsaProvider = null;
+                byte[] signedHash = null;
+                byte[] hashBytes = null;
+
+                // Select target CSP
+                cspParams = new CspParameters();
+                cspParams.ProviderType = 1; // PROV_RSA_FULL
+                                            //cspParams.ProviderName; // CSP name
+                rsaProvider = new RSACryptoServiceProvider(cspParams);
+                // Import public key
+                rsaProvider.FromXmlString(File.ReadAllText(publicKey));
+                // Read binary signed hash file
+                signedHash = File.ReadAllBytes(signedFile);
+                // Read the original hash
+                hashBytes = File.ReadAllBytes(hash);
+                // Check the data
+                return rsaProvider.VerifyHash(hashBytes, CryptoConfig.MapNameToOID("SHA1"), signedHash);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // Encrypt a file
+        public static void EncryptFile(string plainFileName, string publicKeyFileName, string encryptedFileName)
+        {
+            // Variables
+            CspParameters cspParams = null;
+            RSACryptoServiceProvider rsaProvider = null;
+            StreamReader publicKeyFile = null;
+            StreamReader plainFile = null;
+            FileStream encryptedFile = null;
+            string publicKeyText = "";
+            string plainText = "";
+            byte[] plainBytes = null;
+            byte[] encryptedBytes = null;
+            try
+            {
+                // Select target CSP
+                cspParams = new CspParameters();
+                cspParams.ProviderType = 1; // PROV_RSA_FULL
+                //cspParams.ProviderName; // CSP name
+                rsaProvider = new RSACryptoServiceProvider(cspParams);
+                // Read public key from file
+                publicKeyFile = File.OpenText(publicKeyFileName);
+                publicKeyText = publicKeyFile.ReadToEnd();
+                // Import public key
+                rsaProvider.FromXmlString(publicKeyText);
+                // Read plain text from file
+                plainFile = File.OpenText(plainFileName);
+                plainText = plainFile.ReadToEnd();
+                plainFile.Close();
+                // Encrypt plain text
+                plainBytes = Encoding.Unicode.GetBytes(plainText);
+                encryptedBytes = rsaProvider.Encrypt(plainBytes, false);
+                // Write encrypted text to file
+                encryptedFile = File.Create(encryptedFileName);
+                encryptedFile.Write(encryptedBytes, 0, encryptedBytes.Length);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                // Do some clean up if needed
+                if (publicKeyFile != null)
+                {
+                    publicKeyFile.Close();
+                }
+                if (plainFile != null)
+                {
+                    plainFile.Close();
+                }
+                if (encryptedFile != null)
+                {
+                    encryptedFile.Close();
+                }
+            }
+        } // Encrypt
+
+        // Decrypt a file
+        public static void DecryptFile(string encryptedFileName, string privateKeyFileName, string plainFileName)
+        {
+            // Variables
+            CspParameters cspParams = null;
+            RSACryptoServiceProvider rsaProvider = null;
+            StreamReader privateKeyFile = null;
+            FileStream encryptedFile = null;
+            StreamWriter plainFile = null;
+            string privateKeyText = "";
+            string plainText = "";
+            byte[] encryptedBytes = null;
+            byte[] plainBytes = null;
+            try
+            {
+                // Select target CSP
+                cspParams = new CspParameters();
+                cspParams.ProviderType = 1; // PROV_RSA_FULL
+                //cspParams.ProviderName; // CSP name
+                rsaProvider = new RSACryptoServiceProvider(cspParams);
+                // Read private/public key pair from file
+                privateKeyFile = File.OpenText(privateKeyFileName);
+                privateKeyText = privateKeyFile.ReadToEnd();
+                // Import private/public key pair
+                rsaProvider.FromXmlString(privateKeyText);
+                // Read encrypted text from file
+                encryptedFile = File.OpenRead(encryptedFileName);
+                encryptedBytes = new byte[encryptedFile.Length];
+                encryptedFile.Read(encryptedBytes, 0, (int)encryptedFile.Length);
+                // Decrypt text
+                plainBytes = rsaProvider.Decrypt(encryptedBytes, false);
+                // Write decrypted text to file
+                plainFile = File.CreateText(plainFileName);
+                plainText = Encoding.Unicode.GetString(plainBytes);
+                plainFile.Write(plainText);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                // Do some clean up if needed
+                if (privateKeyFile != null)
+                {
+                    privateKeyFile.Close();
+                }
+                if (encryptedFile != null)
+                {
+                    encryptedFile.Close();
+                }
+                if (plainFile != null)
+                {
+                    plainFile.Close();
+                }
+            }
+        } // Decrypt
+    }
+}
